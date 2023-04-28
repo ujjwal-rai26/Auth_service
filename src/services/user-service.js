ec31bb3828f932d54f3933c82a2c2801148f3ba9
ujjwal rai <ujjwalrai26092001@gmail.com>
2023-04-29 00:22:50 +0530

    added jwt create and verify logic

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,8 @@
+const jwt=require('jsonwebtoken');
+
 const  UserRepository  =require('../repository/user-repository');
+
+const {JWT_KEY}=require('../config/serverConfig');
 
 class UserService{
 
@@ -13,6 +17,28 @@ async create(data){
         return user;
     } catch (error) {
         console.log("something went wrong in the service layer");
+        throw {error};
+    }
+}
+
+createToken(user){
+    try {
+       const result =jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
+       return result; 
+    } 
+    catch (error) {
+        console.log("something went wrong in token creation");
+        throw {error};
+    }
+}
+
+verifyToken(token){
+    try {
+       const response =jwt.verify(token,JWT_KEY);
+       return response; 
+    } 
+    catch (error) {
+        console.log("something went wrong in token validation");
         throw {error};
     }
 }
