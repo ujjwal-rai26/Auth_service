35a505c890b22fb70ca0c4140edb35a3da53a624
ujjwal rai <ujjwalrai26092001@gmail.com>
2023-04-28 23:27:45 +0530

    sign up login with incryption login

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express=require('express');
+
+const bodyParser=require('body-parser');
 const app=express();
 
 const { PORT }=require(`./config/serverConfig`);
 
+const apiRoutes =require('./routes/index');
+
 const prepareAndStartServer =()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true})); 
+    
+    app.use('/api',apiRoutes);
+
     app.listen(PORT,()=>{
         console.log(`server started at  ${PORT}`);
     })
